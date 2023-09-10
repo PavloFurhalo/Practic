0723f78ab07bbcbb8f9d2894e3a7da30f92ba266
PavloFurhalo <p.furgalo2006@gmail.com>
2023-09-10 17:44:39 +0300

    Change

--- stack.py
+++ stack.py
@@ -1,11 +1,13 @@
 a =[]
 c = []
+couples = []
+odd =  []
 
 while True:
     b = input("Enter num: ")
     if b.strip() == "w" :
         break
-
+    
     try:
         num = int(b)
     except ValueError:
@@ -17,5 +19,12 @@ while True:
     elif num < 0:
         c.append(num)
 
+    if num % 2 == 0:
+        couples.append(num)
+    elif num % 2 != 0:
+        odd.append(num)
+
 print(a)
 print(c)
+print("Парні: ", couples)
+print("Непарні: " , odd)
