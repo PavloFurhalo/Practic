20910bdf4c65632df01e0f5e71cb030918e9c295
PavloFurhalo <p.furgalo2006@gmail.com>
2023-10-30 22:09:59 +0200

    commit

--- lab5/lab5.js
+++ lab5/lab5.js
@@ -5,12 +5,27 @@ document.getElementById('go').addEventListener('click', function() {
     resultContainer.innerHTML = '';
 
     for (let i = 0; i < sourceText.length; i++) {
-        const letter = sourceText[i].toLowerCase();
-        const isUpperCase = sourceText[i] === sourceText[i].toUpperCase();
-        const imageName = isUpperCase ? `${letter}.png` : `small_${letter}.png`;
-        const image = document.createElement('img');
-        image.src = `images/${imageName}`;
-        image.alt = letter;
-        resultContainer.appendChild(image);
+        const char = sourceText[i];
+
+        if (/^[a-zA-Z]$/.test(char)) {
+            const letter = char.toLowerCase();
+            const isUpperCase = char === char.toUpperCase();
+            const imageName = isUpperCase ? `${letter}.png` : `small_${letter}.png`;
+
+            const image = document.createElement('img');
+            image.src = `images/${imageName}`;
+            image.alt = char;
+            image.style.opacity = '0';
+            resultContainer.appendChild(image);
+
+            const delay = 500;
+
+            setTimeout(function() {
+                image.style.opacity = '1';
+            }, i * delay);
+        } else {
+            const textNode = document.createTextNode(char);
+            resultContainer.appendChild(textNode);
+        }
     }
 });
